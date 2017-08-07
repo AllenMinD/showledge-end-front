c4dc80461b7dcb8fbf9f63597c109fb61ac73707
aLLeNxD <240831608@qq.com>
2017-08-07 15:46:07 +0800

    upload files

--- js/index.js
+++ js/index.js
@@ -1,94 +1,4 @@
-/*index-login.js*/
 $(document).ready(function() {
-	/*----------------------发布学习动态框js----------------------*/
-	/*点击“心情”按钮事件*/
-	$("#input-emotion").click(function(event) {
-		$(".input-emotion-form").slideToggle();
-	});
-
-	/*点击“学习资料”按钮事件*/
-	$("#input-xxzl").click(function(event) {
-		$(".input-xxzl-form").slideToggle();
-	});
-	/*添加学习资料按钮*/
-	$("#add-xxzl").click(function(event) {
-		var newDiv = $("<div></div>");
-		newDiv.addClass("input-xxzl-form input-xxzl-form-extra");
-		newDiv.css({
-			"display": "block",
-			"margin-left": "95px"
-		});
-		var newInput1 = $("<input>");
-		newInput1.attr({
-			"type": "text",
-			"class": "form-control-green-border",
-			"placeholder": "http://"
-		});
-		newDiv.append(newInput1);
-		var newSpan = $("<span></span>");
-		newSpan.addClass("fa fa-external-link");
-		newDiv.append(newSpan);
-		var newInput2 = $("<input>");
-		newInput2.attr({
-			"type": "text",
-			"class": "form-control-green-border",
-			"placeholder": "资料标题"
-		});
-		newDiv.append(newInput2);
-		$(".input-xxbj-form").before(newDiv);
-	});
-
-	/*点击“学习笔记”按钮事件*/
-	$("#input-xxbj").click(function(event) {
-		$(".input-xxbj-form").slideToggle();
-	});	
-
-	/*笔记选择窗口模态框 的选择按钮切换*/
-	$(".pick-btn").click(function(event) {
-		$(this).toggleClass("btn-warning");
-		if ($(this).attr("class") == "btn btn-info btn-sm pick-btn")
-			$(this).text("已选择");
-		else $(this).text("选择");
-	});
-
-	/*笔记选择窗口模态框 的点击“选好了”按钮*/
-	$(".enter-pick").click(function(event) {
-		var $addNoteList = $(".picked-note");
-		$addNoteList.html("");
-		var $pickedNote = $(".pick-btn:not(.btn-warning)");
-		//console.log($pickedNote.prev().text());
-		//console.log($pickedNote.prev().attr("href"));
-		var addNote = "";
-		for (var i = 0; i <  $pickedNote.length; i++) {
-			addNote += '<a href="' + $pickedNote.eq(i).prev().attr("href") + '" target="_blank">《'+ $pickedNote.eq(i).prev().text() +'》</a> ';
-		}
-		$addNoteList.append(addNote);
-		$pickedNote.children('selector').remove();
-	});
-
-
-	/*点击发布按钮*/
-	$("#send-act-btn").on('click', function(event) {
-		event.preventDefault();
-		/* Act on the event */
-		if ($("#input-act").val() == "" || $("#emotion-form").val() == "") alert("请至少填写动态内容和心情");
-		else {
-			/* 发布完动态后初始化各个表单 */
-			$("#input-act").val("");
-			$("#emotion-form").val("");
-			$(".input-xxzl-form > input").val("");
-			$(".picked-note").html("");
-			$(".input-xxzl-form-extra").remove();
-			$(".input-emotion-form").hide();
-			$(".input-xxzl-form").hide();
-			$(".input-xxbj-form").hide();
-			alert("发布成功");
-		} 			
-	});
-
-	/*----------------------发布学习动态框js end----------------------*/
-
-	/*----------------------点击加载按钮 ajax----------------------*/
   function countActiveBind() {
     /*.like-count*/
     $(".like-count").click(function(event) {
@@ -120,7 +30,7 @@ $(document).ready(function() {
   /* 学习动态 - 加载更多按钮 */
   $(".load-more-act").on('click', function(event) {
     event.preventDefault();
-    var $actList = $("#by-act > ul");
+    var $actList = $("#hot-act > ul");
     console.log($actList);
     $.ajax({
       url: 'http://www.easy-mock.com/mock/598723caa1d30433d856d66f/test-data/study-act',
@@ -201,7 +111,7 @@ $(document).ready(function() {
   /* 学习笔记 - 加载更多按钮 */
   $(".load-more-note").on('click', function(event) {
     event.preventDefault();
-    var $noteList = $("#by-note > ul");
+    var $noteList = $("#hot-note > ul");
     console.log($noteList);
     $.ajax({
       url: 'http://www.easy-mock.com/mock/598723caa1d30433d856d66f/test-data/study-note',
@@ -271,7 +181,7 @@ $(document).ready(function() {
   /* 知识地图 - 加载更多按钮 */
   $(".load-more-map").on('click', function(event) {
     event.preventDefault();
-    var $mapList = $("#by-map > ul");
+    var $mapList = $("#hot-map > ul");
     console.log($mapList);
     $.ajax({
       url: 'http://www.easy-mock.com/mock/598723caa1d30433d856d66f/test-data/study-map',
@@ -336,6 +246,5 @@ $(document).ready(function() {
     /*评论按钮、喜欢按钮、收藏按钮激活状态*/
     countActiveBind()     
   });
-});
 
-/*----------------------点击加载按钮 ajax end----------------------*/
+});
